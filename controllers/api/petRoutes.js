ff52256370fe291334efab4ea517cbf6fdc98b9f
Brenda Goodwin <brendanced@gmail.com>
2021-01-06 15:32:42 -0700

    updating

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
     });
     req.session.save(() => {
       req.session.user_id = dbPetData.id;
-      // req.session.loggedIn = true;
+      req.session.loggedIn = true;
 
       res.status(200).json(dbPetData);
     });
@@ -29,41 +29,46 @@ router.post('/', async (req, res) => {
   }
 });
 
-//update profile
-router.put('/', async (req, res) => {
-  try {
-    console.log(req.body);
-    const petData = await Pet.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-    //res.json(petData)
-    res.render(petForm, { pet: petData });
-  } catch (err) {
-    res.json(err);
-  }
+router.get('/', (req, res) => {
+  console.log('homepage FORM');
+  res.render('homepage');
 });
 
-//delete profile
+// //update profile
+// router.put('/', async (req, res) => {
+//   try {
+//     console.log(req.body);
+//     const petData = await Pet.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
+//     //res.json(petData)
+//     res.render(petForm, { pet: petData });
+//   } catch (err) {
+//     res.json(err);
+//   }
+// });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const petData = await Pet.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
+// //delete profile
 
-    if (!petData) {
-      res.status(404).json({ message: 'No pet found with this id!' });
-      return;
-    }
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const petData = await Pet.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
 
-    res.status(200).json(petData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     if (!petData) {
+//       res.status(404).json({ message: 'No pet found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(petData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
