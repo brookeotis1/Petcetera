07b63482c179c98c232d80453331de24ab556ab3
Brooke Maneotis <xmaneotisb@yahoo.com>
2021-01-05 18:51:15 -0700

    routes

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -2,12 +2,12 @@
 
 const router = require('express').Router();
 const { Pet } = require('../../models');
-const { response } = require('express');
+//const { response } = require('express');
 
 router.get('/pet', (req, res) => {
-//   console.log('PET ROUTE FORM');
-//   res.render('petForm');
-// });
+  console.log('PET ROUTE FORM');
+  res.render('petForm');
+});
 
 //create pet profile
 router.post('/', async (req,res) => {
@@ -23,10 +23,43 @@ res.render(petForm, {pet:petData})
 })
 
 //update profile
+router.put('/', async (req,res) => {
+    try {
+        console.log(req.body)
+        const petData = await Pet.create({...req.body, user_id:req.session.user_id});
+//res.json(petData)
+res.render(petForm, {pet:petData})
+    } catch (err) {
+        res.json(err);
+    }
+    
+})
+
 
 
 
 //delete profile
+
+router.delete('/:id', async (req, res) => {
+    try {
+      const petData = await Pet.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!petData) {
+        res.status(404).json({ message: 'No pet found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(petData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 
 
