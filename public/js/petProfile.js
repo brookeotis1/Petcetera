406f9518977393e37385268d5c6212a2d145fd83
Brianne <brianne.ewell1@gmail.com>
2021-01-05 21:29:55 -0700

    cleaned up petform

--- public/js/petProfile.js
+++ public/js/petProfile.js
@@ -1,65 +1,74 @@
 const PetFormHandler = async (event) => {
   event.preventDefault();
+  alert(event);
 
-  const firstName = document.querySelector('#pname').value.trim();
-  const lastName = document.querySelector('#nname').value.trim();
-  const bio = document.querySelector('#species').value.trim();
-  const firstName = document.querySelector('#breed').value.trim();
-  const lastName = document.querySelector('#bdate').value.trim();
-  const bio = document.querySelector('#weight').value.trim();
-  const firstName = document.querySelector('#').value.trim();
-  const lastName = document.querySelector('#bdate').value.trim();
-  const bio = document.querySelector('#weight').value.trim();
+  const petName = document.querySelector('#pname').value.trim();
+  const nickName = document.querySelector('#nname').value.trim();
+  const species = document.querySelector('#species').value.trim();
+  const breed = document.querySelector('#breed').value.trim();
+  const birthdate = document.querySelector('#bdate').value.trim();
+  const weight = document.querySelector('#weight').value.trim();
 
-  if (petName && species) {
-    console.log('made it');
-    const response = await fetch(`/api/pet`, {
-      method: 'POST',
-      body: JSON.stringify({
-        petName,
-        nickname,
-        species,
-        breed,
-        birthdate,
-        weight,
-        allergies,
-        food,
-        microchipId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log(response);
+alert("petname=" + petName);
+alert("nickname=" + nickName);
+alert("species=" + species);
+alert("breed=" + breed);
+alert("birthdate=" + birthdate);
+alert("weight=" + weight);
 
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to create pet profile');
-    }
-  }
-};
+document.getElementById("submitPetForm").submit();
 
-// const delButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
 
-//     const response = await fetch(`/api/ownerProfile/${id}`, {
-//       method: 'DELETE',
+
+}
+  
+
+//   if (petName && species) {
+//     console.log('made it');
+//     const response = await fetch(`/api/pet`, {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         petName,
+//         nickName,
+//         species,
+//         breed,
+//         birthdate,
+//         weight,
+//       }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
 //     });
+//     console.log(response);
 
 //     if (response.ok) {
-//       document.location.replace('/profile');
+//       document.location.replace('/');
 //     } else {
-//       alert('Failed to delete project');
+//       alert('Failed to create pet profile');
 //     }
 //   }
 // };
 
+// // const delButtonHandler = async (event) => {
+// //   if (event.target.hasAttribute('data-id')) {
+// //     const id = event.target.getAttribute('data-id');
+
+// //     const response = await fetch(`/api/ownerProfile/${id}`, {
+// //       method: 'DELETE',
+// //     });
+
+// //     if (response.ok) {
+// //       document.location.replace('/profile');
+// //     } else {
+// //       alert('Failed to delete project');
+// //     }
+// //   }
+// // };
+
 document
   .querySelector('.petProfile-form')
-  .addEventListener('submit', petFormHandler);
+  .addEventListener('submitPetForm', petFormHandler);
 
-// // document
-// //   .querySelector('.ownerProfile-list')
-// //   .addEventListener('click', delButtonHandler);
+// // // document
+// // //   .querySelector('.ownerProfile-list')
+// // //   .addEventListener('click', delButtonHandler);
