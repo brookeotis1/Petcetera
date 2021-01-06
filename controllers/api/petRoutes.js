7a6c0be6708c5d9240ae4419a6ad379826947952
Brenda Goodwin <brendanced@gmail.com>
2021-01-05 17:02:29 -0700

    pet route

---
+++ controllers/api/petRoutes.js
@@ -0,0 +1,8 @@
+// const router = require('express').Router();
+// const { Pet } = require('../../models');
+// const { response } = require('express');
+
+// router.get('/pet', (req, res) => {
+//   console.log('PET ROUTE FORM');
+//   res.render('petForm');
+// });
